b01b072981d127660f33118e5163975197b0eccb
gonzalo10 <gonzalo.achiaga@gmail.com>
2019-04-06 21:44:17 +0200

    backend finished

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+
 const User = require('../../models/user');
 
 module.exports = {
@@ -23,18 +24,22 @@ module.exports = {
       throw err;
     }
   },
-  login: async ({email, password}) => {
+  login: async ({ email, password }) => {
     const user = await User.findOne({ email: email });
     if (!user) {
-        throw new Error('User does not exist!');
+      throw new Error('User does not exist!');
     }
     const isEqual = await bcrypt.compare(password, user.password);
     if (!isEqual) {
-        throw new Error('Passowrd is incorrect!');
+      throw new Error('Password is incorrect!');
     }
-    const token = jwt.sign({ userId: user.id, email: user.email }, 'somesupersecretkey', {
+    const token = jwt.sign(
+      { userId: user.id, email: user.email },
+      'somesupersecretkey',
+      {
         expiresIn: '1h'
-    });
-    return { userId: user.id, token: token, tokenExpiration: 1 }
+      }
+    );
+    return { userId: user.id, token: token, tokenExpiration: 1 };
   }
 };
