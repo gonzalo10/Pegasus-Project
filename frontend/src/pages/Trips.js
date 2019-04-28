74cba07b63e4d65fb5c23c34e29c2b3110e70f16
gonzalo10 <gonzalo.achiaga@gmail.com>
2019-04-28 19:10:55 +0200

    travelpayouts working

--- frontend/src/pages/Trips.js
+++ frontend/src/pages/Trips.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import AuthContext from '../context/auth-context';
 import './Events.css';
+const axios = require('axios');
+
 
 class TripPage extends Component {
   state = {
@@ -14,49 +16,48 @@ class TripPage extends Component {
   }
 
   componentDidMount() {
-		this.fetchEvents();
-		fetch("https://api.travelpayouts.com/data/cities.json",
-		 {
-			 credentials:"include",
-				 headers:{
-				 'X-Access-Token': '269dd9a31218fab6b4e5eb7d10c41003'
-				},
-				referrerPolicy:"no-referrer-when-downgrade",
-				body:null,
-				method:"GET",
-				mode:"no-cors"})
-		.then(res => {
-			if (res.success !== true) {
-				throw new Error(res);
-			}
-			return res.json();
-		})
-		.then(resData => {
-			console.log('primera opcion',resData)
-		})
-		.catch(err => {
-			console.log('primera opcion',JSON.stringify(err));
-		});
-		fetch("https://api.travelpayouts.com/v1/prices/cheap?origin=MOW&destination=HKT&depart_date=2019-11&return_date=2019-12&token=269dd9a31218fab6b4e5eb7d10c41003",
-		 {
-				 headers:{
-				 'X-Access-Token': '269dd9a31218fab6b4e5eb7d10c41003'
-				},
-				method:"GET",
-				mode:"no-cors"})
-		.then(res => {
-			if (res.success !== true) {
-				throw new Error(res);
-			}
-			return res.json();
-		})
-		.then(resData => {
-			console.log('tercera opcion',resData)
-		})
-		.catch(err => {
-			console.log('tercera opcion',JSON.stringify(err));
-		});
-
+		//this.fetchEvents();
+		// fetch("https://api.travelpayouts.com/data/cities.json",
+		//  {
+		// 	 credentials:"include",
+		// 		 headers:{
+		// 		 'X-Access-Token': '269dd9a31218fab6b4e5eb7d10c41003'
+		// 		},
+		// 		referrerPolicy:"no-referrer-when-downgrade",
+		// 		body:null,
+		// 		method:"GET",
+		// 		mode:"no-cors"})
+		// .then(res => {
+		// 	if (res.success !== true) {
+		// 		throw new Error(res);
+		// 	}
+		// 	return res.json();
+		// })
+		// .then(resData => {
+		// 	console.log('primera opcion',resData)
+		// })
+		// .catch(err => {
+		// 	console.log('primera opcion',JSON.stringify(err));
+		// });
+		// fetch("https://api.travelpayouts.com/v1/prices/cheap?origin=MOW&destination=HKT&depart_date=2019-11&return_date=2019-12&token=269dd9a31218fab6b4e5eb7d10c41003",
+		//  {
+		// 		 headers:{
+		// 		 'X-Access-Token': '269dd9a31218fab6b4e5eb7d10c41003'
+		// 		},
+		// 		method:"GET",
+		// 		mode:"no-cors"})
+		// .then(res => {
+		// 	if (res.success !== true) {
+		// 		throw new Error(res);
+		// 	}
+		// 	return res.json();
+		// })
+		// .then(resData => {
+		// 	console.log('tercera opcion',resData)
+		// })
+		// .catch(err => {
+		// 	console.log('tercera opcion',JSON.stringify(err));
+		// });
 	}
 
 
