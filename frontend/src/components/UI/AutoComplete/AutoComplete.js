29a405abb0341e45e7dd7760aa4964a052d75623
Achiaga <alfonso.achiaga@gmail.com>
2019-05-27 23:53:49 +0900

    Add Autocomplete

--- frontend/src/components/UI/AutoComplete/AutoComplete.js
+++ frontend/src/components/UI/AutoComplete/AutoComplete.js
@@ -1,11 +1,61 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 export class Autocomplete extends Component {
+  
+  static propTypes = {
+    options: PropTypes.instanceOf(Array).isRequired
+  };
+
+  state = {
+    activeOption: 0,
+    filteredOptions: [],
+    showOptions: false,
+    userInput: ''
+  };
+
+  onChange = (e) => {
+    const { options } = this.props;
+    const userInput = e.currentTarget.value;
+    const filteredOptions = options.filter(
+          (option) => option.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+        );
+    this.setState({
+          activeOption: 0,
+          filteredOptions,
+          showOptions: true,
+          userInput
+        });
+      };
+
+  onClick = (e) => {
+    this.setState({
+      activeOption: 0,
+      filteredOption: [],
+      showOptions: false,
+      userInput: e.currentTarget.innerText
+    });
+  };
+
   render() {
+    const {
+      onChange,
+      onKeyDown,
+      state: { activeOption, filteredOptions, showOptions, userInput }
+    } = this;
+    let optionList;
     return (
       <React.Fragment>
         <div className="search">
-          <input type="text" className="search-box" />
+          <input
+            type="text"
+            className="search-box"
+            onChange={onChange}
+            onKeyDown={onKeyDown}
+            value={userInput}
+          />
           <input type="submit" value="" className="search-btn" />
+          {optionList}
         </div>
       </React.Fragment>
     );
--- frontend/src/containers/FrontPageIntro/FrontPageIntro.js
+++ frontend/src/containers/FrontPageIntro/FrontPageIntro.js
@@ -1,16 +1,32 @@
 import React from 'react';
 
 import Monkey from '../../assets/img/Monkey.gif'
+import Autocomplete from '../../components/UI/AutoComplete/AutoComplete'
+
 import './FrontPageIntro.css'
  
 const introPage = (props) => {
+    
     return(
         <div className='IntroPage'>
             <h1>Intro</h1>
             <p>Intro de la Pagina</p>
             <img src={Monkey} alt='Working on it'/>
+            <Autocomplete
+                options={[
+                'Papaya',
+                'Persimmon',
+                'Paw Paw',
+                'Prickly Pear',
+                'Peach',
+                'Pomegranate',
+                'Pineapple'
+                ]}
+            />
         </div>
     )
 }
+
+
  
 export default introPage;
